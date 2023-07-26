c1c68e5ecb509a8fc39ebc6cdee0bfab4c27d49a
DEEPIKA V <136463509+deepika1242@users.noreply.github.com>
2023-07-26 23:23:15 +0530

    mag_com_4bit.v

---
+++ MAGNITUDE COMPARATOR/mag_com_4bit.v
@@ -0,0 +1,26 @@
+module mag_com_4bit(
+  input [3:0]A,B,
+    output reg G,E,L
+    );
+  always @ (A,B)
+    begin 
+      if (A<B)
+		  begin
+        G=0;
+        E=0;
+        L=1;
+				end  
+      else if (A==B)
+		  begin
+        G=0;
+        E=0;
+        L=1;
+			end  				
+		  else
+		  begin
+        G=1;
+        E=0;
+        L=0;
+			  end  
+end  
+endmodule
